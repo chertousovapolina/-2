ecb40eec0e142e232d0585238cd2ed4ee7c209b6
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-16 17:49:07 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+﻿// Chertousova PE
+// 16.10.2022
+// Task 5
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int W, H, R, D;
+	cin>> W >> H >> R; 
+	D = R * 2;
+	if (D <= W && D<= H) {
+		cout << "YES";
+	}
+	else { cout << "NO"; 
+	}
+}
